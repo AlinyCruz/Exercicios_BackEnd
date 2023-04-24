415e21fda6e2932321dbce7a62e04317747bc070
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-04-24 13:56:58 -0300

    incluindo diretorio msc-architecture-trybecar

---
+++ msc-architecture-trybecar/src/models/index.js
@@ -0,0 +1,11 @@
+const passengerModel = require('./passenger.model');
+const travelModel = require('./travel.model');
+const waypointModel = require('./waypoint.model');
+const driverModel = require('./driver.model');
+
+module.exports = {
+  passengerModel,
+  travelModel,
+  waypointModel,
+  driverModel,
+};
