9183d0fe34095351b2fab3cf6682588cb2bb2bcb
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-05-05 19:13:01 -0300

    Fixando aprendizado dia 02 ORM - partes

---
+++ app-with-sequelize-dia-02/src/models/employee.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true 
+    },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'employees',
+    underscored: true, // tranforma camelCase em Snake_case
+  });
+
+  Employee.associate = (models) => {
+    Employee.hasOne(models.Address,
+      { 
+        foreignKey: 'employeeId',
+        as: 'addresses' 
+      });
+  };
+
+  return Employee;
+};
